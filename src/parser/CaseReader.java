db4cb27171747b065dd396cbf66547b27a45a8f2
Arnfnn Gjørvad <arnfinnr@stud.ntnu.no>
2013-03-14 15:07:11 +0100

    Readcases

---
+++ src/parser/CaseReader.java
@@ -0,0 +1,22 @@
+package parser;
+
+public class CaseReader {
+	public static void main(String[] args) {
+		String[] cases = readCases();
+		for(String s:cases)
+			System.out.println(s);
+	}
+	public static String[] readCases(){
+		String[] returns = new String[8];
+		for(int i = 1; i<9;i++){
+			String file = Utility.readFile("Data/Case"+i);
+			returns[i-1] = file;
+		}
+		return returns;
+	}
+	public static String[] parseCases(String[] cases){
+		
+		return cases;
+		
+	}
+}
