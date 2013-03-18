5322b15dd2c31e38a88fbb47ca316acc424648b4
Arnfnn Gjørvad <arnfinnr@stud.ntnu.no>
2013-03-18 13:24:28 +0100

    -xmlparser and html stripper

--- src/parser/CaseReader.java
+++ src/parser/CaseReader.java
@@ -3,9 +3,11 @@ package parser;
 public class CaseReader {
 	public static void main(String[] args) {
 		String[] cases = readCases();
+		cases = parseCases(cases);
 		for(String s:cases)
 			System.out.println(s);
 	}
+	
 	public static String[] readCases(){
 		String[] returns = new String[8];
 		for(int i = 1; i<9;i++){
@@ -15,7 +17,6 @@ public class CaseReader {
 		return returns;
 	}
 	public static String[] parseCases(String[] cases){
-		
 		return cases;
 		
 	}
