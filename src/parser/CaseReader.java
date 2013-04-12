ee2c3de19c5ae5554380912d08a3ee7a48c3c2cc
Arnfnn Gjørvad <arnfinnr@stud.ntnu.no>
2013-04-12 14:09:21 +0200

    -cleaned up a little

--- src/parser/CaseReader.java
+++ src/parser/CaseReader.java
@@ -1,5 +1,9 @@
 package parser;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
 import org.apache.lucene.analysis.Analyzer;
 import org.apache.lucene.analysis.no.NorwegianAnalyzer;
 import org.apache.lucene.util.Version;
@@ -16,11 +20,33 @@ public class CaseReader {
 	public static Case[] readCases(){
 		Case[] returns = new Case[8];
 		for(int i = 1; i<9;i++){
-			String file = Utility.readFile("Data/Cases/Case"+i);
+			String file = readFile("Data/Cases/Case"+i);
 			returns[i-1] = new Case(file);
 		}
 		return returns;
 	}
+	private static String readFile(String fileName) {
+		BufferedReader br = null;
+		StringBuilder sb = new StringBuilder();
+		try {
+			String sCurrentLine;
+			br = new BufferedReader(new FileReader(fileName));
+			while ((sCurrentLine = br.readLine()) != null) {
+				sb.append(sCurrentLine+" ");
+			}
+			
+		} catch (IOException e) {
+			e.printStackTrace();
+		} finally {
+			try {
+				if (br != null)br.close();
+			} catch (IOException ex) {
+				ex.printStackTrace();
+			}
+		}
+		return sb.toString();
+	}
+
 	public static Case[] parseCases(Case[] cases){
 		return cases;
 		
