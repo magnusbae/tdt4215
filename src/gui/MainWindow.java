eabb9a6a54d239068dcdccc18eaa1dace0cf57f2
Magnus Bae <magnusbae@gmail.com>
2013-04-19 11:29:36 +0200

    added a new button

--- src/gui/MainWindow.java
+++ src/gui/MainWindow.java
@@ -9,6 +9,10 @@ import org.eclipse.swt.widgets.Button;
 import org.eclipse.swt.custom.StyledText;
 import org.eclipse.swt.events.SelectionAdapter;
 import org.eclipse.swt.events.SelectionEvent;
+import org.eclipse.swt.events.MouseAdapter;
+import org.eclipse.swt.events.MouseEvent;
+import org.eclipse.swt.events.KeyAdapter;
+import org.eclipse.swt.events.KeyEvent;
 
 public class MainWindow {
 
@@ -24,10 +28,23 @@ public class MainWindow {
 		shell.setText("SWT Application");
 		
 		Button btnSk = new Button(shell, SWT.NONE);
-		btnSk.setBounds(10, 86, 584, 28);
-		btnSk.setText("S\u00F8k");
+		btnSk.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseUp(MouseEvent e) {
+				
+				//TODO search!
+			}
+		});
+		btnSk.setBounds(10, 86, 420, 28);
+		btnSk.setText("Søk");
 		
 		StyledText searchBox = new StyledText(shell, SWT.BORDER);
+		searchBox.addKeyListener(new KeyAdapter() {
+			@Override
+			public void keyPressed(KeyEvent e) {
+				//TODO search on control + enter
+			}
+		});
 		searchBox.addSelectionListener(new SelectionAdapter() {
 			
 			/*
@@ -43,7 +60,7 @@ public class MainWindow {
 		
 		
 		
-		searchBox.setText("Skriv eller lim inn s\u00F8ketekst her");
+		searchBox.setText("Skriv eller lim inn søketekst her");
 		searchBox.setBounds(10, 10, 584, 72);
 		
 		StyledText styledText_1 = new StyledText(shell, SWT.BORDER);
@@ -51,6 +68,15 @@ public class MainWindow {
 		styledText_1.setEditable(false);
 		styledText_1.setBackground(SWTResourceManager.getColor(SWT.COLOR_WIDGET_LIGHT_SHADOW));
 		styledText_1.setBounds(10, 120, 584, 304);
+		
+		Button btnNewButton = new Button(shell, SWT.NONE);
+		btnNewButton.addSelectionListener(new SelectionAdapter() {
+			@Override
+			public void widgetSelected(SelectionEvent e) {
+			}
+		});
+		btnNewButton.setBounds(437, 86, 157, 28);
+		btnNewButton.setText("Søk i eksempelcaser");
 
 		shell.open();
 		shell.layout();
